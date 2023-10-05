1794202b926256f8dff5366a90bd668ab81a89ff
Sourcery AI <>
2023-10-05 08:55:33 +0000

    'Refactored by Sourcery'

--- starcoder.cpp/convert-hf-to-ggml.py
+++ starcoder.cpp/convert-hf-to-ggml.py
@@ -41,16 +41,12 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 model_name = sys.argv[1].strip()
-fname_out = "models/" + sys.argv[1].strip() + "-ggml.bin"
+fname_out = f"models/{sys.argv[1].strip()}-ggml.bin"
 os.makedirs(os.path.dirname(fname_out), exist_ok=True)
 
 
 
-# use 16-bit or 32-bit floats
-use_f16 = True
-if len(sys.argv) > 2:
-    use_f16 = False
-
+use_f16 = len(sys.argv) <= 2
 print("Loading model: ", model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
@@ -69,144 +65,141 @@ encoder.update(tokenizer.get_added_vocab())
 print(hparams)
 
 print("Saving ggml model to: ", fname_out)
-fout = open(fname_out, "wb")
+with open(fname_out, "wb") as fout:
+    fout.write(struct.pack("i", 0x67676d6c)) # magic: ggml in hex
+    vocab_size = hparams["vocab_size"]
+    fout.write(struct.pack("i", vocab_size))
+    # fout.write(struct.pack("i", len(encoder)))
+    fout.write(struct.pack("i", hparams["n_positions"])) # n_ctx
+    fout.write(struct.pack("i", hparams["n_embd"]))
+    fout.write(struct.pack("i", hparams["n_head"]))
+    fout.write(struct.pack("i", hparams["n_layer"]))
+    fout.write(struct.pack("i", use_f16))
 
-fout.write(struct.pack("i", 0x67676d6c)) # magic: ggml in hex
-vocab_size = hparams["vocab_size"]
-fout.write(struct.pack("i", vocab_size))
-# fout.write(struct.pack("i", len(encoder)))
-fout.write(struct.pack("i", hparams["n_positions"])) # n_ctx
-fout.write(struct.pack("i", hparams["n_embd"]))
-fout.write(struct.pack("i", hparams["n_head"]))
-fout.write(struct.pack("i", hparams["n_layer"]))
-fout.write(struct.pack("i", use_f16))
+    byte_encoder = bytes_to_unicode()
+    byte_decoder = {v:k for k, v in byte_encoder.items()}
 
-byte_encoder = bytes_to_unicode()
-byte_decoder = {v:k for k, v in byte_encoder.items()}
+    fout.write(struct.pack("i", vocab_size))
 
-fout.write(struct.pack("i", vocab_size))
+    counter = 0
+    # sort by value
+    for key in sorted(encoder, key=encoder.get):
+        text = bytearray([byte_decoder[c] for c in key])
+        fout.write(struct.pack("i", len(text)))
+        fout.write(text)
+        counter += 1
 
-counter = 0
-# sort by value
-for key in sorted(encoder, key=encoder.get):
-    text = bytearray([byte_decoder[c] for c in key])
-    fout.write(struct.pack("i", len(text)))
-    fout.write(text)
-    counter += 1
+    # TODO: Repeat last token until vocab_size
+    while counter < vocab_size:
+        fout.write(struct.pack("i", len(text)))
+        fout.write(text)
+        counter += 1
+    # assert counter == config.vocab_size
 
-# TODO: Repeat last token until vocab_size
-while counter < vocab_size:
-    fout.write(struct.pack("i", len(text)))
-    fout.write(text)
-    counter += 1
-# assert counter == config.vocab_size
+    for name in list_vars.keys():
+        data = list_vars[name].squeeze().numpy()
+        print(f"Processing variable: {name} with shape: ", data.shape)
 
-for name in list_vars.keys():
-    data = list_vars[name].squeeze().numpy()
-    print("Processing variable: " + name + " with shape: ", data.shape)
-
-    # rename headers to keep compatibility
-    if name == "transformer.ln_f.weight":
-        name = "model/ln_f/g"
-    elif name == "transformer.ln_f.bias":
-        name = "model/ln_f/b"
-    elif name == "transformer.wte.weight":
-        name = "model/wte"
-    elif name == "transformer.wpe.weight":
-        name = "model/wpe"
-    elif name == "lm_head.weight":
-        name = "model/lm_head"
-    elif re.match(r"transformer.h\.\d+\.ln_1\.weight", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/ln_1/g"
-    elif re.match(r"transformer.h\.\d+\.ln_1\.bias", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/ln_1/b"
-    elif re.match(r"transformer.h\.\d+\.attn\.c_attn\.weight", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/attn/c_attn/w"
-    elif re.match(r"transformer.h\.\d+\.attn\.c_attn\.bias", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/attn/c_attn/b"
-    elif re.match(r"transformer.h\.\d+\.attn\.c_proj\.weight", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/attn/c_proj/w"
-    elif re.match(r"transformer.h.\d+.attn.c_proj.bias", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/attn/c_proj/b"
-    elif re.match(r"transformer.h.\d+.ln_2.weight", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/ln_2/g"
-    elif re.match(r"transformer.h.\d+.ln_2.bias", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/ln_2/b"
-    elif re.match(r"transformer.h.\d+.mlp.c_fc.weight", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/mlp/c_fc/w"
-    elif re.match(r"transformer.h.\d+.mlp.c_fc.bias", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/mlp/c_fc/b"
-    elif re.match(r"transformer.h.\d+.mlp.c_proj.weight", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/mlp/c_proj/w"
-    elif re.match(r"transformer.h.\d+.mlp.c_proj.bias", name):
-        i = re.findall("\d+", name)[0]
-        name = f"model/h{i}/mlp/c_proj/b"
-    else:
-        print("Unrecognized variable name. %s", name)
-
-    # we don't need these
-    if name.endswith("attn.masked_bias") or name.endswith(".attn.bias"):
-        print("  Skipping variable: " + name)
-        continue
-
-    n_dims = len(data.shape);
-
-    # ftype == 0 -> float32, ftype == 1 -> float16
-    ftype = 0;
-    if use_f16:
-        if (name == "model/wte" or name == "model/lm_head" or name[-2:] == "/g" or name[-2:] == "/w") and n_dims == 2:
-            print("  Converting to float16")
-            data = data.astype(np.float16)
-            ftype = 1
+        # rename headers to keep compatibility
+        if name == "transformer.ln_f.weight":
+            name = "model/ln_f/g"
+        elif name == "transformer.ln_f.bias":
+            name = "model/ln_f/b"
+        elif name == "transformer.wte.weight":
+            name = "model/wte"
+        elif name == "transformer.wpe.weight":
+            name = "model/wpe"
+        elif name == "lm_head.weight":
+            name = "model/lm_head"
+        elif re.match(r"transformer.h\.\d+\.ln_1\.weight", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/ln_1/g"
+        elif re.match(r"transformer.h\.\d+\.ln_1\.bias", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/ln_1/b"
+        elif re.match(r"transformer.h\.\d+\.attn\.c_attn\.weight", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/attn/c_attn/w"
+        elif re.match(r"transformer.h\.\d+\.attn\.c_attn\.bias", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/attn/c_attn/b"
+        elif re.match(r"transformer.h\.\d+\.attn\.c_proj\.weight", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/attn/c_proj/w"
+        elif re.match(r"transformer.h.\d+.attn.c_proj.bias", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/attn/c_proj/b"
+        elif re.match(r"transformer.h.\d+.ln_2.weight", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/ln_2/g"
+        elif re.match(r"transformer.h.\d+.ln_2.bias", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/ln_2/b"
+        elif re.match(r"transformer.h.\d+.mlp.c_fc.weight", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/mlp/c_fc/w"
+        elif re.match(r"transformer.h.\d+.mlp.c_fc.bias", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/mlp/c_fc/b"
+        elif re.match(r"transformer.h.\d+.mlp.c_proj.weight", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/mlp/c_proj/w"
+        elif re.match(r"transformer.h.\d+.mlp.c_proj.bias", name):
+            i = re.findall("\d+", name)[0]
+            name = f"model/h{i}/mlp/c_proj/b"
         else:
-            print("  Converting to float32")
-            data = data.astype(np.float32)
-            ftype = 0
+            print("Unrecognized variable name. %s", name)
 
-    "model/h.*/attn/c_attn/w"
-    "model/h.*/attn/c_proj/w"
-    "model/h.*/mlp/c_fc/w"
-    "model/h.*/mlp/c_proj/w"
-    if name[-14:] == "/attn/c_attn/w" or name[-14:] == "/attn/c_attn/b":
-        print("  Duplicate K,V heads to use MHA instead of MQA")
+            # we don't need these
+        if name.endswith("attn.masked_bias") or name.endswith(".attn.bias"):
+            print(f"  Skipping variable: {name}")
+            continue
 
-        embed_dim = hparams["n_embd"]
-        head_dim = embed_dim // hparams["n_head"]
+        n_dims = len(data.shape);
 
-        # ((n_heads + 2) * head_dim, hidden_dim) -> (3 * n_heads * head_dim, hidden_dim)
-        q, k ,v = np.split(data, (hparams["n_head"] * head_dim, (hparams["n_head"] + 1) * head_dim), axis=0)
-        # duplicate k, v along the first axis (head_dim, hidden_dim) -> (n_heads * head_dim, hidden_dim)
-        if len(k.shape) == 2:
-            k = np.tile(k, (hparams["n_head"], 1))
-            v = np.tile(v, (hparams["n_head"], 1))
-        elif len(k.shape) == 1:
-            k = np.tile(k, (hparams["n_head"]))
-            v = np.tile(v, (hparams["n_head"]))
-        # concat q, k, v along the first axis (n_heads * head_dim, hidden_dim) -> (3 * n_heads * head_dim, hidden_dim)
-        data = np.concatenate((q, k, v), axis=0)
+        # ftype == 0 -> float32, ftype == 1 -> float16
+        ftype = 0;
+        if use_f16:
+            if (name == "model/wte" or name == "model/lm_head" or name[-2:] == "/g" or name[-2:] == "/w") and n_dims == 2:
+                print("  Converting to float16")
+                data = data.astype(np.float16)
+                ftype = 1
+            else:
+                print("  Converting to float32")
+                data = data.astype(np.float32)
+                ftype = 0
 
-    # header
-    str = name.encode('utf-8')
-    fout.write(struct.pack("iii", n_dims, len(str), ftype))
-    for i in range(n_dims):
-        fout.write(struct.pack("i", data.shape[n_dims - 1 - i]))
-    fout.write(str);
+        "model/h.*/attn/c_attn/w"
+        "model/h.*/attn/c_proj/w"
+        "model/h.*/mlp/c_fc/w"
+        "model/h.*/mlp/c_proj/w"
+        if name[-14:] in ["/attn/c_attn/w", "/attn/c_attn/b"]:
+            print("  Duplicate K,V heads to use MHA instead of MQA")
 
-    # data
-    data.tofile(fout)
+            embed_dim = hparams["n_embd"]
+            head_dim = embed_dim // hparams["n_head"]
 
-fout.close()
+            # ((n_heads + 2) * head_dim, hidden_dim) -> (3 * n_heads * head_dim, hidden_dim)
+            q, k ,v = np.split(data, (hparams["n_head"] * head_dim, (hparams["n_head"] + 1) * head_dim), axis=0)
+            # duplicate k, v along the first axis (head_dim, hidden_dim) -> (n_heads * head_dim, hidden_dim)
+            if len(k.shape) == 2:
+                k = np.tile(k, (hparams["n_head"], 1))
+                v = np.tile(v, (hparams["n_head"], 1))
+            elif len(k.shape) == 1:
+                k = np.tile(k, (hparams["n_head"]))
+                v = np.tile(v, (hparams["n_head"]))
+            # concat q, k, v along the first axis (n_heads * head_dim, hidden_dim) -> (3 * n_heads * head_dim, hidden_dim)
+            data = np.concatenate((q, k, v), axis=0)
 
-print("Done. Output file: " + fname_out)
+        # header
+        str = name.encode('utf-8')
+        fout.write(struct.pack("iii", n_dims, len(str), ftype))
+        for i in range(n_dims):
+            fout.write(struct.pack("i", data.shape[n_dims - 1 - i]))
+        fout.write(str);
+
+        # data
+        data.tofile(fout)
+
+print(f"Done. Output file: {fname_out}")
 print("")
